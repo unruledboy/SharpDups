b614d7d3e4acc4a176b57844bf81f2d4965943d6
Wilson Chen <unruledboy@users.noreply.github.com>
2018-07-24 20:20:02 +1000

    Add files via upload

--- Logic/IDupDetector.cs
+++ Logic/IDupDetector.cs
@@ -5,6 +5,6 @@ namespace Xnlab.SharpDups.Logic
 {
     public interface IDupDetector
     {
-		(List<Duplicate> duplicates, IList<string> failedToProcessFiles) Find(IEnumerable<string> files, int workers);
+		(List<Duplicate> duplicates, IList<string> failedToProcessFiles) Find(IEnumerable<string> files, int workers, int bufferSize = 0);
     }
 }
