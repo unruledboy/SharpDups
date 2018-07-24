ee3b7f7680ab271b2115d4e8deec31b9a49dca33
Wilson Chen <unruledboy@users.noreply.github.com>
2018-07-24 18:19:07 +1000

    Update DupDetectorV3.cs

--- Logic/DupDetectorV3.cs
+++ Logic/DupDetectorV3.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,131 +9,201 @@ using Xnlab.SharpDups.Model;
 
 namespace Xnlab.SharpDups.Logic
 {
-    public class DupDetectorV3 : IDupDetector
-    {
-        private const int BufferSize = 4096;
-        private int _workers;
+	public class ProgressiveDupDetector : IDupDetector
+	{
+		private const int BufferSize = 4096;
+		private int _workers;
 
-        public List<Duplicate> Find(IEnumerable<string> files, int workers)
-        {
-            _workers = workers;
+		public (List<Duplicate> duplicates, IList<string> failedToProcessFiles) Find(IEnumerable<string> files, int workers)
+		{
+			_workers = workers;
 
-            if (_workers <= 0)
-                _workers = 5;
+			if (_workers <= 0)
+				_workers = 5;
 
-            var result = new List<Duplicate>();
+			var result = new List<Duplicate>();
+			var failedToProcessFiles = new List<string>();
 
-            //groups with same file size
-            var sameSizeGroups = files.Select(f =>
-            {
-                var info = new FileInfo(f);
-                return new DupItem { FileName = f, ModifiedTime = info.LastWriteTime, Size = info.Length };
-            }).GroupBy(f => f.Size).Where(g => g.Count() > 1);
+			//groups with same file size
+			var sameSizeGroups = files.Select(f =>
+			{
+				return GetDupFileItem(f);
+			}).GroupBy(f => f.Size).Where(g => g.Count() > 1);
 
-            var mappedSameSizeGroupList = new ConcurrentBag<IGrouping<string, DupItem>>();
+			var mappedSameSizeGroupList = new ConcurrentBag<IGrouping<string, DupItem>>();
 
-            Parallel.ForEach(MapFileSizeGroups(sameSizeGroups), mappedSameSizeGroups =>
-            {
-                foreach (var group in mappedSameSizeGroups)
-                {
-                    foreach (var file in group)
-                    {
-                        if (file.Size > 0)
-                        {
-                            //fast random byte checking
-                            using (var stream = File.OpenRead(file.FileName))
-                            {
-                                var length = stream.Length;
-                                file.Tags = new byte[3];
-                                //first byte
-                                stream.Seek(0, SeekOrigin.Begin);
-                                file.Tags[0] = (byte)stream.ReadByte();
+			Parallel.ForEach(MapFileSizeGroups(sameSizeGroups), mappedSameSizeGroups =>
+			{
+				foreach (var group in mappedSameSizeGroups)
+				{
+					foreach (var file in group)
+					{
+						try
+						{
+							//fast random byte checking
+							QuickHashFile(file);
+						}
+						catch (Exception)
+						{
+							failedToProcessFiles.Add(file.FileName);
+						}
+					}
 
-                                //middle byte, we need it especially for xml like files
-                                if (length > 1)
-                                {
-                                    stream.Seek(stream.Length / 2, SeekOrigin.Begin);
-                                    file.Tags[1] = (byte)stream.ReadByte();
-                                }
+					//groups with same quick hash value
+					var sameQuickHashGroups = group.GroupBy(f => f.QuickHash).Where(g => g.Count() > 1);
+					foreach (var sameQuickHashGroup in sameQuickHashGroups)
+					{
+						mappedSameSizeGroupList.Add(sameQuickHashGroup);
+					}
+				}
+			});
 
-                                //last byte
-                                if (length > 2)
-                                {
-                                    stream.Seek(0, SeekOrigin.End);
-                                    file.Tags[2] = (byte)stream.ReadByte();
-                                }
+			Parallel.ForEach(MapFileHashGroups(mappedSameSizeGroupList), mappedSameHashGroups =>
+			{
+				foreach (var quickHashGroup in mappedSameHashGroups)
+				{
+					ProgressiveHash(quickHashGroup);
 
-                                file.QuickHash = HashTool.GetHashText(file.Tags);
-                            }
-                        }
-                    }
+					//phew, finally.....
+					//group by same file hash
+					var sameFullHashGroups = quickHashGroup.Where(g => !g.IsDifferent).GroupBy(g => g.FullHash).Where(g => g.Count() > 1);
+					result.AddRange(sameFullHashGroups.Select(fullHashGroup => new Duplicate { Items = fullHashGroup.Select(f => new FileItem { FileName = f.FileName, ModifiedTime = f.ModifiedTime, Size = f.Size }) }));
+				}
+			});
 
-                    //groups with same quick hash value
-                    var sameQuickHashGroups = group.GroupBy(f => f.QuickHash).Where(g => g.Count() > 1);
-                    foreach (var sameQuickHashGroup in sameQuickHashGroups)
-                    {
-                        mappedSameSizeGroupList.Add(sameQuickHashGroup);
-                    }
-                }
-            });
+			return (result, failedToProcessFiles);
+		}
 
-            Parallel.ForEach(MapFileHashGroups(mappedSameSizeGroupList), mappedSameHashGroups =>
-            {
-                foreach (var quickHashGroup in mappedSameHashGroups)
-                {
-                    IncrementalHash(quickHashGroup);
+		private static DupItem GetDupFileItem(string f)
+		{
+			var info = new FileInfo(f);
+			return new DupItem { FileName = f, ModifiedTime = info.LastWriteTime, Size = info.Length };
+		}
 
-                    //phew, finally.....
-                    //group by same file hash
-                    var sameFullHashGroups = quickHashGroup.GroupBy(g => g.FullHash).Where(g => g.Count() > 1);
-                    result.AddRange(sameFullHashGroups.Select(fullHashGroup => new Duplicate { Items = fullHashGroup.Select(f => new FileItem { FileName = f.FileName, ModifiedTime = f.ModifiedTime, Size = f.Size }) }));
-                }
-            });
+		public static DupItem ProgressiveHashFile(string file)
+		{
+			var dupFileItem = GetDupFileItem(file);
+			QuickHashFile(dupFileItem);
+			var length = dupFileItem.Size / BufferSize;
+			if (length == 0)
+				length = 1;
+			var position = 0;
+			for (var i = 0; i < length; i++)
+			{
+				ProgressiveHashSection(position, dupFileItem);
+				position += BufferSize;
+			}
+			return dupFileItem;
+		}
 
-            return result;
-        }
+		public static bool ProgressiveCompareFile(DupItem sourceDupItem, string targetFile)
+		{
+			var targetDupFileItem = GetDupFileItem(targetFile);
+			if (targetDupFileItem.Size != sourceDupItem.Size)
+				return false;
+			QuickHashFile(targetDupFileItem);
+			var length = targetDupFileItem.Size / BufferSize;
+			if (length == 0)
+				length = 1;
+			var position = 0;
+			for (var i = 0; i < length; i++)
+			{
+				ProgressiveHashSection(position, targetDupFileItem);
+				if (sourceDupItem.HashSections.Count < i + 1 || targetDupFileItem.HashSections[i] != sourceDupItem.HashSections[i])
+					return false;
+				position += BufferSize;
+			}
+			return true;
+		}
 
-        private void IncrementalHash(IGrouping<string, DupItem> quickHashGroup)
-        {
-            var groups = quickHashGroup.ToArray();
-            var first = groups.First();
-            var length = first.Size / BufferSize;
-            if (length == 0)
-                length = 1;
-            var position = 0;
-            for (var i = 0; i < length; i++)
-            {
-                position += BufferSize;
-                foreach (var group in groups.GroupBy(g => i == 0 ? string.Empty : g.HashSections[i - 1]))
-                {
-                    foreach (var groupFile in group)
-                    {
-                        if (groupFile.HashSections == null)
-                            groupFile.HashSections = new List<string>();
-                        groupFile.HashSections.Add(HashTool.HashFile(groupFile.FileName, position, BufferSize));
-                    }
-                }
-            }
+		private static void QuickHashFile(DupItem file)
+		{
+			if (file.Size > 0)
+			{
+				using (var stream = File.Open(file.FileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+				{
+					var length = stream.Length;
+					file.Tags = new byte[3];
+					//first byte
+					stream.Seek(0, SeekOrigin.Begin);
+					file.Tags[0] = (byte)stream.ReadByte();
 
-            foreach (var groupFile in groups)
-            {
-                groupFile.FullHash = string.Join(string.Empty, groupFile.HashSections);
-            }
-        }
+					//middle byte, we need it especially for xml like files
+					if (length > 1)
+					{
+						stream.Seek(stream.Length / 2, SeekOrigin.Begin);
+						file.Tags[1] = (byte)stream.ReadByte();
+					}
 
-        private IEnumerable<IEnumerable<IGrouping<long, DupItem>>> MapFileSizeGroups(IEnumerable<IGrouping<long, DupItem>> source)
-        {
-            var it = source.ToArray();
-            var size = it.Length / _workers;
-            return it.Section(size);
-        }
+					//last byte
+					if (length > 2)
+					{
+						stream.Seek(0, SeekOrigin.End);
+						file.Tags[2] = (byte)stream.ReadByte();
+					}
 
-        private IEnumerable<IEnumerable<IGrouping<string, DupItem>>> MapFileHashGroups(IEnumerable<IGrouping<string, DupItem>> source)
-        {
-            var it = source.ToArray();
-            var size = it.Length / _workers;
-            return it.Section(size);
-        }
+					file.QuickHash = HashTool.GetHashText(file.Tags);
+				}
+			}
+		}
 
-    }
+		private void ProgressiveHash(IGrouping<string, DupItem> quickHashGroup)
+		{
+			var groups = quickHashGroup.ToArray();
+			var first = groups.First();
+			var length = first.Size / BufferSize;
+			if (length == 0)
+				length = 1;
+			var position = 0;
+			for (var i = 0; i < length; i++)
+			{
+				foreach (var group in groups.Where(g => !g.IsDifferent).GroupBy(g => i == 0 ? string.Empty : g.HashSections[i - 1]))
+				{
+					var hashCount = 0;
+					foreach (var groupFile in group)
+					{
+						ProgressiveHashSection(position, groupFile);
+						hashCount = groupFile.HashSections.Count;
+					}
+
+					foreach (var incrementalGroupWithSameHashSection in group.GroupBy(g => g.HashSections[hashCount - 1]))
+					{
+						if (incrementalGroupWithSameHashSection.Count() == 1)
+						{
+							foreach (var item in incrementalGroupWithSameHashSection)
+							{
+								item.IsDifferent = true;
+							}
+						}
+					}
+				}
+				position += BufferSize;
+			}
+
+			foreach (var groupFile in groups.Where(g => !g.IsDifferent))
+			{
+				groupFile.FullHash = string.Join(string.Empty, groupFile.HashSections);
+			}
+		}
+
+		private static void ProgressiveHashSection(int position, DupItem dupItem)
+		{
+			if (dupItem.HashSections == null)
+				dupItem.HashSections = new List<string>();
+			dupItem.HashSections.Add(HashTool.HashFile(dupItem.FileName, position, BufferSize));
+		}
+
+		private IEnumerable<IEnumerable<IGrouping<long, DupItem>>> MapFileSizeGroups(IEnumerable<IGrouping<long, DupItem>> source) => Slice(source);
+
+		private IEnumerable<IEnumerable<IGrouping<T, DupItem>>> Slice<T>(IEnumerable<IGrouping<T, DupItem>> source)
+		{
+			var it = source.ToArray();
+			var size = it.Length / _workers;
+			if (size == 0)
+				size = 1;
+			return it.Section(size);
+		}
+
+		private IEnumerable<IEnumerable<IGrouping<string, DupItem>>> MapFileHashGroups(IEnumerable<IGrouping<string, DupItem>> source) => Slice(source);
+	}
 }
