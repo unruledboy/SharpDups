0eaf453f769ad32008d8541f875f87041cf65393
Wilson Chen <unruledboy@users.noreply.github.com>
2018-07-25 08:30:08 +1000

    Update IDupDetector.cs

--- Logic/IDupDetector.cs
+++ Logic/IDupDetector.cs
@@ -3,8 +3,8 @@ using Xnlab.SharpDups.Model;
 
 namespace Xnlab.SharpDups.Logic
 {
-    public interface IDupDetector
-    {
+	public interface IDupDetector
+	{
 		(List<Duplicate> duplicates, IList<string> failedToProcessFiles) Find(IEnumerable<string> files, int workers, int quickHashSize = 3, int bufferSize = 0);
-    }
+	}
 }
