5e1b4251b2e517c3318d09bd79a1b2b3745f0512
Wilson Chen <unruledboy@users.noreply.github.com>
2018-07-25 08:31:22 +1000

    Update ExtensionMethods.cs

--- Infrastructure/ExtensionMethods.cs
+++ Infrastructure/ExtensionMethods.cs
@@ -1,30 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Xnlab.SharpDups.Infrastructure
 {
-    public static class ExtensionMethods
-    {
-        public static IEnumerable<IEnumerable<T>> Section<T>(this IEnumerable<T> source, int length)
-        {
-            if (length <= 0)
-                throw new ArgumentOutOfRangeException("length");
+	public static class ExtensionMethods
+	{
+		public static IEnumerable<IEnumerable<T>> Section<T>(this IEnumerable<T> source, int length)
+		{
+			if (length <= 0)
+				throw new ArgumentOutOfRangeException("length");
 
-            var section = new List<T>(length);
+			var section = new List<T>(length);
 
-            foreach (var item in source)
-            {
-                section.Add(item);
+			foreach (var item in source)
+			{
+				section.Add(item);
 
-                if (section.Count == length)
-                {
-                    yield return section.AsReadOnly();
-                    section = new List<T>(length);
-                }
-            }
+				if (section.Count == length)
+				{
+					yield return section.AsReadOnly();
+					section = new List<T>(length);
+				}
+			}
 
-            if (section.Count > 0)
-                yield return section.AsReadOnly();
-        }
-    }
+			if (section.Count > 0)
+				yield return section.AsReadOnly();
+		}
+	}
 }
