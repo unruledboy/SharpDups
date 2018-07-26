faac71c11add463ea02f96814c0c0380e5d30c11
Wilson Chen <unruledboy@users.noreply.github.com>
2018-07-26 20:54:06 +1000

    Add files via upload

--- Model/DupResult.cs
+++ Model/DupResult.cs
@@ -4,10 +4,11 @@ namespace Xnlab.SharpDups.Model
 {
 	public class DupResult
 	{
-		public long TotalFiles { get; set; }
-		public long TotalFileBytes { get; set; }
+		public long TotalComparedFiles { get; set; }
+		public long TotalBytesInComparedFiles { get; set; }
 		public long TotalReadBytes { get; set; }
 		public List<Duplicate> Duplicates { get; set; }
 		public List<string> FailedToProcessFiles { get; set; }
+		public long TotalFiles { get; set; }
 	}
 }
